13c53e0955d71eef237144a4001fd170cbbd0f26
oshita-n <oshita-n@whispon.com>
2021-06-15 05:00:18 +0900

    🔨 矢印の追加

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Footer } from '../components/Footer.js'
 import { Header } from '../components/Header.js'
 import Link from 'next/link'
-
 import 'tailwindcss/tailwind.css'
+import { ArrowNarrowDownIcon } from '@heroicons/react/solid'
+
 
 
 export default function Home() {
@@ -12,8 +13,9 @@ export default function Home() {
       <main className="container mx-auto">
         <div>
           <h1 className="sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl mt-10"><Link href="/">AiPrismTechnologies</Link></h1>
-          <main className="flex justify-center items-center container mx-auto　w-screen h-screen">
-            <h1　className="sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl -mt-40">サービス開発を通じて、お客様に感動を提供します。</h1>
+          <main className="grid justify-items-center container mx-auto　w-screen h-screen">
+            <h1　className="sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl mt-60">サービス開発を通じて、お客様に感動を提供します。</h1>
+            <ArrowNarrowDownIcon className="animate-bounce w-6 h-6" />
           </main>
           <h1 className="sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl xl:mt-10">お問い合わせ</h1>
           <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-gray-600">AiPrismTechnologiesはAIに関する受託開発をしており、クライアントの課題解決をサポートします。気軽なご相談からでも受け付けております。</p>  
