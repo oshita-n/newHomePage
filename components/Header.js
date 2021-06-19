2ee91c986983e0f782c62a0b14ec1df82c296552
oshita-n　　 <oshita-n@whispon.com>
2021-06-20 03:59:55 +0900

    🔨 GoogleAnalyticsのための情報を追加

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,15 @@ export  function Header() {
         <title>AiPrism</title>
         <meta name="description" content="今まで見えなかったものを可視化し、価値を提供する" />
         <link rel="icon" href="/prism.png" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECKBCC52BY"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ECKBCC52BY');
+        </script>
     </Head>
   )
 }
